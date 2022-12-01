af64ffb262c699773fe97e41c0ebd4b01b54d715
TheNeoStormZ <73660500+TheNeoStormZ@users.noreply.github.com>
2022-12-01 19:12:35 +0100

    [TEST] Creado test de trad id a esp
    Se comprueba que se hace correctamente
    
    Co-Authored-By: Francisco J Migueles Student <99977786+Framigdom@users.noreply.github.com>

--- decide/booth/tests.py
+++ decide/booth/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from voting.models import Voting, Question
+from django.utils import timezone
+from mixnet.models import Auth
+from django.conf import settings
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+from base.tests import BaseTestCase
+
+
+class BoothTranslationTestCase(StaticLiveServerTestCase):
+
+    def setUp(self):
+        q = Question(desc = 'test question')
+        q.save()
+
+        v = Voting(name='test voting', question=q)
+        v.save()
+        a, _ = Auth.objects.get_or_create(url=settings.BASEURL,
+                                          defaults={'me': True, 'name': 'test auth'})
+        a.save()
+        v.auths.add(a)
+        v.create_pubkey()
+        v.start_date = timezone.now()
+        v.save()
+
+        self.v_id = v.id
+
+        super().setUp()    
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+        
+
+    def tearDown(self):           
+        super().tearDown()
+        self.driver.quit()
+        self.base.tearDown()
+
+    def testCheckIDTransES(self):
+        self.driver.get(f'{self.live_server_url}/booth/'+str(self.v_id))
+        title = self.driver.find_elements(By.TAG_NAME, 'h1')[1].text
+        title = title.split(": ")[0]
+        print("'"+ title + "'")     
+        return self.assertEqual(str(title),'ID de la votación')
