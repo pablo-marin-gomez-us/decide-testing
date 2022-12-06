90c63ca8168a8b4cf9171b534cf50cc650511187
alvmorcal1 <alvmorcal1@alum.us.es>
2022-12-06 14:20:19 +0100

    [Desarrollo] Correcta Visualización de los votos multiopción. Ahora la vista de Visualizer interpreta correctamente los votos multiopción, mostrando claramente que opción ha sido la más prioritaria para los votantes.

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -16,6 +16,24 @@ class VisualizerView(TemplateView):
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
+
+            if r[0]['question']['multioption']:
+                postpro = r[0]['postproc']
+                position_votes = {}
+                for i,votes in enumerate(postpro):
+                    position_votes[i] = votes['votes']
+
+                sorted_votes = dict(sorted(position_votes.items(), key=lambda item:item[1]))
+                order = list(sorted_votes.values())
+
+                for index,key in enumerate(sorted_votes.keys()):
+                    order[key] = index+1
+                context['order'] = order
+                for index,opt in enumerate(r[0]['postproc']):
+                    r[0]['postproc'][index]['order'] = order[index]
+                    
+                context['voting'] = json.dumps(r[0])
+
         except:
             raise Http404
 
