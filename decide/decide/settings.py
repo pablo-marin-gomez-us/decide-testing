43576ac8676b3a0e514685e307dcf140e2d33d7a
pabalvcar <pabloalvarezcaro@gmail.com>
2022-12-13 20:04:44 +0100

    [Desarrollo] Añadido botón logout
    Además, se han protegido los secrets de las aplicaciones OAuth de GitHub y Twitter.

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -213,10 +214,10 @@ if os.path.exists("config.jsonnet"):
 #OAUTH
 
 SOCIAL_AUTH_GITHUB_KEY = 'd781104d572cee72044d'
-SOCIAL_AUTH_GITHUB_SECRET = 'fd224f92a08f2fd84062936c082876fc5c901cc2'
+SOCIAL_AUTH_GITHUB_SECRET = config('SOCIAL_AUTH_GITHUB_SECRET', default='')
 
-SOCIAL_AUTH_TWITTER_KEY = 'YNDBxTdIi6qS3QL0OcNlMZxZY'
-SOCIAL_AUTH_TWITTER_SECRET = 'JDNATR2mFrvIMg7u6eVltEwI3WyGgF96g2VRpoIsISVLcyezuj'
+SOCIAL_AUTH_TWITTER_KEY = 'DVfT2oobVZ0fS904CYpc20ODT'
+SOCIAL_AUTH_TWITTER_SECRET = config('SOCIAL_AUTH_TWITTER_SECRET', default='')
 
 
 USE_X_FORWARDED_HOST = True
